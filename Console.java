89a0db88459a004d2b5ff20d119cbece99826f3f
zirodev23 <renars.sirotins@proton.me>
2026-01-09 09:46:22 +0200

    broken..

---
+++ Console.java
@@ -0,0 +1,29 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Console {
+    public static float readInput(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextFloat();
+    }
+
+    public static float readInput(String prompt, int lowLimit, int highLimit) {
+        while (true) {
+            System.out.print(prompt);
+
+            try {
+                input = scanner.nextFloat();
+
+                if (input >= highLimit && input <= lowLimit)
+                    break;
+
+                System.out.print(errorMessage);
+            } catch (InputMismatchException e) {
+                System.out.println("Nederīga ievade. Lūdzu, ievadiet skaitli.");
+                scanner.next(); // Consume the invalid input
+            }
+        }
+
+        return input;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.print("|===> Hipotekārā kredīta kalkulators <===|\n");
+
+            int totalCreditSum = Console.readInput(
+                "Ievadiet kredīta summu: ", 1_000, 1_000_000, "Kredīta summai jābūt robežās no 1'000 līdz 1'000'000\n"
+            );
+
+            float yearlyPercentageRate =  Console.readInput(
+                "Ievadiet gada procentu likmi: ", 0, 30, "Procentu likmei jābūt robežās no 0 līdz 30\n"
+            );
+
+            int years = Console.readInput(
+                "Ievadiet periodu gados: ", 1, 30, "Periodam jābūt robežās no 1 līdz 30\n"
+            );
+
+            var mortgageCalculator = new MortgageCalculator();
+            System.out.println("Ikmēneša maksājums: " + mortgageCalculator.calculate(totalCreditSum, yearlyPercentageRate, years) + " EUR");
+
+            if (Console.readInput("Vai vēlaties turpināt? Jā[1]/Nē[0]: ") == 0)
+                break;
+        }
+
+        return mortgageCalculator;
+    }
+}
